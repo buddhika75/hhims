238ac989ea797befa2b65bf090a6f1b5eb6b2497
Dr. M. H. B. Ariyaratne <buddhika.ari@gmail.com>
2015-03-23 17:32:01 +0530

    Merge origin/master
    Conflicts:
    	pom.xml
    	src/main/java/com/divudi/bean/hr/HrReportController.java
    	src/main/java/com/divudi/bean/hr/ShiftFingerPrintAnalysisController.java
    	src/main/java/com/divudi/bean/hr/StaffAdditionalFormController.java
    	src/main/java/com/divudi/bean/pharmacy/PharmacyItemExcelManager.java
    	src/main/java/com/divudi/data/dataStructure/WeekDayWork.java
    	src/main/resources/META-INF/persistence.xml
    	src/main/setup/glassfish-resources.xml
    	src/main/webapp/WEB-INF/glassfish-web.xml
    	src/main/webapp/WEB-INF/web.xml
    	src/main/webapp/hr/hr_shift_table_finger_print.xhtml
    	src/main/webapp/hr/hr_staff_leave_entitle.xhtml
    	src/main/webapp/resources/css/pharmacypos.css
    	src/main/webapp/resources/pharmacy/saleBill_five_five.xhtml
    	src/main/webapp/resources/template/template.xhtml

---
+++ src/main/java/hotel/enums/DepartmentOrInstitutionType.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.enums;
+
+/**
+ *
+ * @author Buddhika
+ */
+public enum DepartmentOrInstitutionType {
+    Hospital,
+    Ward,
+    Unit,
+    Clinic,
+    Opd,
+    Lab,
+    Radiology,
+    Pharmacy,
+    Manufacturer,
+    Company,
+    Supplier,
+    Room,
+    Hall,
+    Kitchen,
+    Stores,
+    Office,
+    Other,
+}
